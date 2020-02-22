dfd6d27685463d0c242d2413825585c32f512897
Anders Juengst <anders.juengst@teradata.com>
2020-02-22 12:31:24 -0500

    Module models

--- modules/models.py
+++ modules/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Module(models.Model):
+    video_link = models.URLField(max_length=250)
+    title = models.CharField(max_length=500)
+    created = models.DateField(auto_now=False, auto_now_add=True)
+    last_updated = models.DateField(auto_now=True, auto_now_add=False)
+    description = models.TextField()
+    task_list = models.ForeignKey('TaskList', on_delete=models.CASCADE)
+
+class TaskList(models.Model):
+    sample = models.CharField(max_length=200)
